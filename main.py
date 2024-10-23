435709ba3a8919146885686dfbff2de9ea3b5799
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-10-22 23:58:33 -0300

    fix: query for search is correct

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 
 url = "https://myanimelist.net/topanime.php"
 
 response = requests.get(url)
 
-soup = BeautifulSoup(response.content, "lxml")
+soup = BeautifulSoup(response.content, "html.parser")
 
 animes = []
-for item in soup.select(".ranking-list .detail .di-ib a"):
+for item in soup.select(".ranking-list .detail .di-ib a.hoverinfo_trigger"):
     title = item.text.strip()
-    animes.append(title)
+    url = item.get("href")
+    animes.append((title, url))
 
-animes = [item for item in animes if item and item.strip()]
-
-df = pd.DataFrame(animes, columns=["Title"])
-print(df)
+print(animes)
